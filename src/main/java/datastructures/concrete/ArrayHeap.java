58aa5545c00556c5420467390a21992ca4a9ff82
huyhoangk21 <kuhpro@gmail.com>
2017-11-11 22:13:03 -0800

    array heap

--- src/main/java/datastructures/concrete/ArrayHeap.java
+++ src/main/java/datastructures/concrete/ArrayHeap.java
@@ -5,12 +5,13 @@ import misc.exceptions.EmptyContainerException;
 import misc.exceptions.NotYetImplementedException;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 /**
  * See IPriorityQueue for details on what each method must do.
  */
 public class ArrayHeap<T extends Comparable<T>> implements IPriorityQueue<T> {
-    // See spec: you must implement a implement a 4-heap.
+    // See spec: you must implement a implement a 3-heap.
     private static final int NUM_CHILDREN = 4;
 
     // You MUST use this field to store the contents of your heap.
@@ -19,18 +20,15 @@ public class ArrayHeap<T extends Comparable<T>> implements IPriorityQueue<T> {
     private T[] heap;
 
     // Feel free to add more fields and constants.
-    private int numOfElements;
-    private int height;
-    
+    private int size;
+
     public ArrayHeap() {
-        heap = makeArrayOfT(5);
-        numOfElements = 0;
-        height = 2;
+        heap = makeArrayOfT(1);
+        size = 0;
     }
 
     /**
-     * This method will return a new, empty array of the given size
-     * that can contain elements of type T.
+     * This method will return a new, empty array of the given size that can contain elements of type T.
      *
      * Note that each element in the array will initially be null.
      */
@@ -46,69 +44,102 @@ public class ArrayHeap<T extends Comparable<T>> implements IPriorityQueue<T> {
 
     @Override
     public T removeMin() {
-        T min = heap[0];
-        heap[0] = heap[numOfElements - 1];
-        heap[numOfElements - 1] = null;
-        rectifyOrderRemove();
-        numOfElements--;
-        return min;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        } else {
+            T output = this.peekMin();
+            heap[0] = heap[size - 1];
+            // percolate down
+            int k = 0;
+            for (int i = 0; i < height(); i++) {
+                int minChildIndex = NUM_CHILDREN * k + 1;
+                if (minChildIndex < size) {
+                    T minChild = heap[minChildIndex];
+
+                    int childIndex = minChildIndex;
+                    // find the minimum child
+                    for (int j = 0; j < NUM_CHILDREN - 1; j++) {
+                        childIndex++;
+                        if ((childIndex < size) && (minChild.compareTo(heap[childIndex]) >= 0)) {
+                            minChildIndex = childIndex;
+                            minChild = heap[minChildIndex];
+                        }
+                    }
+                    if (heap[k].compareTo(minChild) >= 0) {
+                        T newChild = heap[k];
+                        heap[minChildIndex] = newChild;
+                        heap[k] = minChild;
+                        k = minChildIndex;
+                    }
+                }
+            }
+            size--;
+            return output;
+        }
     }
 
     @Override
     public T peekMin() {
-        return heap[0];
+        if (size == 0) {
+            throw new NoSuchElementException();
+        } else {
+            return heap[0];
+        }
     }
 
     @Override
     public void insert(T item) {
-    	if (numOfElements == heap.length) {
-    		increaseArraySize();
-    	}
-    	int index = numOfElements;    		
-        heap[index] = item;
-        numOfElements++;
-        rectifyOrderInsert(index);
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        // resize
+        resize(size + 1);
+
+        // insert
+        heap[size] = item;
+
+        // percolate up
+        int k = size;
+        for (int i = height(); i >= 0; i--) {
+            T newlyInsertedChild = heap[k];
+            int parentIndex = (k - 1) / NUM_CHILDREN;
+            T parent = heap[parentIndex];
+            if (parent.compareTo(newlyInsertedChild) >= 0) {
+                heap[parentIndex] = newlyInsertedChild;
+                heap[k] = parent;
+            }
+            k = parentIndex;
+        }
+        size++;
     }
 
     @Override
     public int size() {
-        return numOfElements;
+        return size;
     }
-    
-    private void increaseArraySize() {
-    	// increases size by a row in the heap
-    	int newSize = size() + (int)Math.pow(4, height);
-    	T[] oldHeap = heap;
-    	heap = makeArrayOfT(newSize);
-    	for (int i = 0; i < oldHeap.length; i++) {
-    		heap[i] = oldHeap[i];
-    	}
-    	height++;
+
+    // helper methods
+    public int height() {
+        return (int) Math.ceil(((Math.log(size * NUM_CHILDREN - size + 1) / Math.log(NUM_CHILDREN)) - 1));
     }
-    
-    private int getParentIndex(int index) {
-    	if (index % 4 == 0) {
-    		return (index - 4) / 4;
-    	} else {
-    		return (index - (index % 4)) / 4;
-    	}
+
+    private void resize(int capacity) {
+        if (capacity + 1 > heap.length) {
+            int newCapacity = capacity * 2;
+            T[] newHeap = makeArrayOfT(newCapacity);
+            for (int i = 0; i < capacity; i++) {
+                newHeap[i] = heap[i];
+            }
+            heap = newHeap;
+        }
     }
-    
-    private void rectifyOrderRemove() {
-    	
+
+    public String toString() {
+        String result = "Heap: ";
+        for (int i = 0; i < size; i++) {
+            result += heap[i] + " ";
+        }
+        return result;
     }
-    
-    private void rectifyOrderInsert(int index) {
-    	
-    	if (index != 0) {
-	    	int parentIndex = getParentIndex(index);
-	    	T parent = heap[parentIndex];
-	    	T item = heap[index];
-	    	if (parent.compareTo(item) > 0) {
-	    		heap[parentIndex] = item;
-	    		heap[index] = parent;
-	    		rectifyOrderInsert(parentIndex);
-	    	}
-    	}
-    }
+
 }
